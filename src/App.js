fb1d639ee33c9774ec70c911b1cc6422e6c95c32
Millana <chotubaevam@gmail.com>
2022-02-13 19:15:27 +0600

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = ({
+      minValue: 0,
+      maxValue: 0,
+      randomNum: 0
+    })
+  }
+  handlerMin(e) {
+    let min = isNaN(e.target.value) ? 0 : e.target.value
+    this.setState({ minValue: min})
+  }
+  handlerMax(e) {
+    let max = isNaN(e.target.value) ? 0 : e.target.value
+    this.setState({ maxValue: max})
+  }
+  handlerSubmit(e) {
+    e.preventDefault();
+    let newRandomNum = Math.floor(Math.random() * (parseInt(this.state.maxValue) - parseInt(this.state.minValue) + 1) + parseInt(this.state.minValue))
+    this.setState({ randomNum: newRandomNum })
+  }
+  handleClean() {
+    this.setState = ({
+      minValue: 0,
+      maxValue: 0,
+      randomNum: 0
+    })
+  }
+  render() {
+    return (
+      <>
+        <div className='container'>
+          <h2>Random Number :<span>{this.state.randomNum}</span></h2>
+          <form onSubmit={(e) => this.handlerSubmit(e)}>
+            <fieldset>
+              <div>Lower Limit<input type='number' min="1" placeholder="min value" value={this.state.minValue} onChange={(e) => this.handlerMin(e)} /></div>
+              <div>Upper Limit<input type='number' min="1" value={this.state.maxValue} onChange={(e) => this.handlerMax(e)} /></div>
+              <div>
+                <button className='btn generate' type='submit'>Generate</button>
+                <button className='btn clean' type='button' onClick={() => this.handleClean()}>Clean</button>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </>
+    )
+  }
 }
+export default App
 
-export default App;
